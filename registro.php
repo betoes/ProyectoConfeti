84b6b0b4915b19f0c75d0896a3512cf42590bc83
betoes <ragest031@gmail.com>
2019-03-14 17:04:47 -0600

    Estructura de carpetas del Proyecto y se añadio el Registro de usuario

---
+++ registro.php
@@ -0,0 +1,63 @@
+<?php session_start();
+    if (isset($_SESSION['nombre'])) {
+        header('Location: index.php');
+        die();
+    }
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $nombre = filter_var(strtolower($_POST['nombre']), FILTER_SANITIZE_STRING);
+        $apellido = filter_var(strtolower($_POST['apellido']), FILTER_SANITIZE_STRING);
+        $correo = filter_var($_POST['correo'], FILTER_SANITIZE_EMAIL);
+        $password = $_POST['password'];
+        $password2 = $_POST['password2'];
+        $telefono = filter_var($_POST['telefono'], FILTER_SANITIZE_STRING);
+        $errores = '';
+
+        $dsn = 'mysql:dbname=test,host=localhost';
+        $username = 'root';
+        $pass = '';
+        
+
+        if(empty($nombre) or empty($apellido) or empty($correo) or empty($telefono) or empty($password) or empty($password2)){
+            $errores .= '<li>Por favor rellena todos los datos correctamente</li>';
+        } else {
+            try{
+                $conn = new PDO('mysql:host=localhost;dbname=test', $username, $pass);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e){
+                echo "ERROR: " . $e->getMessage();
+            }
+                
+            $statement = $conn->prepare('SELECT * FROM usuario WHERE nombre = :nombre LIMIT 1');
+            $statement -> execute(array(':nombre' => $nombre));
+
+            $resultado = $statement->fetch();
+
+            if($resultado != false){
+                $errores .= '<li>El nombre de usuario ya existe</li>';
+            }
+
+            //CODIFICAR CONTRASEÑAS
+
+            if($password != $password2) {
+                $errores .= '<li>Las contraseñas no coinciden</li>';
+            }
+        }
+
+        if($errores == ''){
+            $statement = $conn->prepare(
+                'INSERT INTO usuario (nombre, apellido, correo, password, password2, telefono)
+                 VALUES (:nombre, :apellido, :correo, :password, :password2, :telefono)'
+                );
+            $statement->execute(array(
+                ':nombre' => $nombre,
+                ':apellido' => $apellido,
+                ':correo' => $correo,
+                ':password' => $password,
+                ':password2' => $password2,
+                ':telefono' => $telefono
+            ));
+
+        }
+    }
+    require_once 'views/registro.view.php';
+?>
---
+++ views/registro.view.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Formulario Contacto</title>
+    <link rel="stylesheet" href="estilos.css">
+</head>
+<body>
+
+    <div class="side-image"></div>
+
+    <div class="formulario-container">
+
+        <h1 class="titulo">Registro de usuario</h1>
+        <form class="formulario" name="login" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>" method="POST">
+            <input type="text" placeholder="Nombre" class="nombre" name="nombre">
+            <input type="text" placeholder="Apellido" class="apellido" name="apellido"> 
+            <input type="email" placeholder="Correo" class="correo" name="correo">
+            <input type="password" placeholder="Contraseña" class="password" name="password">
+            <input type="password" placeholder="Repetir contraseña" class="password2" name="password2">
+            <input type="text" placeholder="Telefono" class="telefono" name="telefono">
+            <input type="submit" onclick="login.submit()" value="Enviar">
+
+            <!--Errores-->
+            <?php if(!empty($errores)): ?>
+                <div class="error">
+                    <ul>
+                        <?php echo $errores; ?>
+                    </ul>
+                </div>
+            <?php endif; ?>
+
+        </form>
+    </div>
+
+</body>
+
+</html>
