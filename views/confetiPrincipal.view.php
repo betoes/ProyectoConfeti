60768f8d95e4420272342ea680b1257efd3abe19
jeth18 <36642059+jeth18@users.noreply.github.com>
2019-03-27 16:02:31 -0600

    Se subio lo del primer sprint completo

---
+++ views/confetiPrincipal.view.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"></meta>
+    <meta name = "viewport" content="width-device-width">
+    <meta name = "author" content="Jethran Enrique">
+    <link rel="stylesheet" type="text/css" href="css/estiloInicio.css">
+    <<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <title>Confeti</title>
+  </head>
+  <body>
+    <div id="contenedor">
+        <div id="logo">
+            <h1>
+              <span class="titulo">Confeti</span>
+            </h1>
+        </div>
+        
+        <div id="boton">
+          <a href="cerrar.php" id="bCerrar" >Cerrar sesion</a>
+        </div>
+        
+        <div id="imagen">
+          <i class="fa fa-user-circle fa-4x" aria-hidden="true"></i>
+        </div>
+
+    </div>
+  </body>
+</html>
