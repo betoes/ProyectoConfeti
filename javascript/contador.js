af42077d4b7b43bbeb7a539ddbed04836c7c97aa
ang0823 <angelsanchez934@gmail.com>
2019-04-12 13:26:30 -0500

    Se editó el contador de la emisión

--- javascript/contador.js
+++ javascript/contador.js
@@ -4,15 +4,15 @@ window.onload = function move() {
 
   var length = 0;
   
-  var id = setInterval(frame, 1000);
+  var id = setInterval(frame, 10);
   
   function frame() {
-    if (length == 100) {
+    if (length > 100) {
       clearInterval(id);
       alert('Se acabo el tiempo!');
       elem.style.width = "0%";
     } else {
-      length = length + 10; 
+      length = length + .1; 
       elem.style.width = length + '%'; 
     }
   }
